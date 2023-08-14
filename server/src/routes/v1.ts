cccbc3ed685ab201c2c096a2862d8a46bad97b6b
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2023-08-14 13:49:10 +0000

    adding  routes for riddles

--- server/src/routes/v1.ts
+++ server/src/routes/v1.ts
@@ -1,10 +1,31 @@
 import { Request, Response, Router } from 'express';
-const {db } = require('../firebase');
+const { db } = require('../firebase');
 
 const api_v1 = Router();
 
-api_v1.get('/', (req:Request, res:Response) => {
+api_v1.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Riddler API v1.0.0');
+});
+
+api_v1.get('/riddles', async (req: Request, res: Response) => {
+  try {
+    const riddlesSnapshot = await db.collection('riddles').get();
+    const riddles: any[] = [];
+
+    riddlesSnapshot.forEach((doc: { id: any; data: () => any }) => {
+      riddles.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    console.log(riddles); // Log the retrieved riddles for debugging purposes
+
+    res.send({ riddles: riddles });
+  } catch (error) {
+    console.error('Error fetching riddles:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 export default api_v1;
